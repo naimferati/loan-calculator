bd66eb4a461a8faddd645af12eeffe1806d8cb4b
Naim Ferati <naim.ferati@inteligenta.io>
2022-03-06 17:51:45 +0100

    Initial project!

---
+++ src/test/java/com/naimferati/loancalculator/LoanCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naimferati.loancalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
